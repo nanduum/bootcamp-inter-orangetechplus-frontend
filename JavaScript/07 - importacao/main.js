c9223eb63fbf4cd5de49c18cfb9f69e4a5183f47
Fernando Andrade <116602686+nanduum@users.noreply.github.com>
2022-10-26 16:18:21 -0300

    att arquivos da pasta 07

--- JavaScript/07 - importacao/main.js
+++ JavaScript/07 - importacao/main.js
@@ -1,6 +1,22 @@
 
 const { gets, print } = require('./funcoes-auxiliares');
 
+/*
+Uma Sala contem 5 alunos e para cada aluno foi sorteaddo um número de 1 - 100.
+Faça um programa que receba os 5 números sorteados para os alunos e mostre
+o maior numero sorteado.
+
+Dados de entrada:
+5
+50
+10
+98
+23
+
+Saída:
+98
+*/
+
 const quantidadeDeAlunos = gets();
 let maiorValorEncontrado = 0;
 
